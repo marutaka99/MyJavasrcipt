43eb801c893894629533d656f93f01cd74e64861
marutaka99 <45503662+marutaka99@users.noreply.github.com>
2018-12-24 17:09:20 +0900

    15 add npm eslint (#20)
    * 15 git test branch
    
    *  15 add text
    
    * 15 add npm run eslint
    
    * 15 add program history

---
+++ js/15test.js
@@ -0,0 +1,17 @@
+
+let ary =[ 'javascript', 'Ajax' ,'ASP.NET']
+console.log(delete ary[0]);
+console.log(ary);
+
+let data1 = 1;
+console.log(delete data1);
+console.log(data1);
+
+data2 = 10;
+console.log(delete data2);
+// console.log(data2);
+
+console.log('==============================');
+
+const num = 1 ;
+console.log(typeof num);
