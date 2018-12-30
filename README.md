b4deb077b7d7358041c82e9480bc93e3f6a98550
marutaka99 <maruktaka99@yahoo.co.jp>
2018-12-30 15:21:08 +0900

    16 学習用にスクリプトを記載

--- README.md
+++ README.md
@@ -1,70 +1,56 @@
-1.
+# test
+1. console.logにHelloWorldを出力
   - 2018/12/1
     - 01HelloWorld.html
-      - console.logにHelloWorldを出力
-2.
+2. 01のjavascript処理を外部ファイルに移動
   - 2018/12/1
     - 02HelloWorld.html
     - helloworld.js
-       - 01のjavascript処理を外部ファイルに移動
-3.
+3. 引数に名前を取得し、別ファイルのスクリプトから、名前を画面出力
   - 2018/12/1
     - 03message.html
     - greeting.js
-      - 引数に名前を取得し、別ファイルのスクリプトから、名前を画面出力
-4.
+4. エスケープ文字を反映
   - 2018/12/1
     - 03message.html
     - greeting.js
-      - エスケープ文字を反映	
-5.
+5. テキストボックスの入力値をボタンクリックで、表示する
   - 2018/12/1
     - 05textbox.html
     - buttonclick.js
-      - テキストボックスの入力値をボタンクリックで、表示する	
-6.
+6. 上記ファイルにcssを当てる
   - 2018/12/1
     - 05textbox.html
-      - 上記ファイルにcssを当てる
-7.
+7. 簡易電卓を作成
   - 2018/12/2
     - 07calculator.html
     - calculator.js
-      - 簡易電卓を作成
-8.
+8. キーボードからの文字入力を取得
   - 2018/12/2
     - 08inputKeyboard.html
     - inputKeyboard.js
-          - キーボードからの文字入力を取得
-9.
+9. p要素をボタンクリックで追加、削除処理
   - 2018/12/2
     - 09addtext.html
-    - addtext.js	
-      - p要素をボタンクリックで追加、削除処理
-10.
-  - 2018/12/9
-    - calculator.js
-      - 電卓のキーボード入力に対応
-11.
-  - 2018/12/9
-    - 09addtext.html
-    - addtext.js	
-    - design9	
-      - p要素に、div要素と、クリックして、変化するように変更
-12.
-  - 2018/12/16
-    - 10addList.html
-    - addList.js	
-      - テキストボックスとプルダウンを<li>要素で追加する処理
-13.
-  - 2018/12/16
-    - 13dice.html
-    - design13.css	
-    - dice.js	
-      - サイコロを作成
-14.
-  - 2018/12/24
-    - npm run eslintを設定
-15.
-  - 2018/12/24
-    - npm run eslinｔ設定用にnpm install
+    - addtext.js
+10. 電卓のキーボード入力に対応
+    - 2018/12/9
+      - calculator.js
+11. p要素に、div要素と、クリックして、変化するように変更
+    - 2018/12/9
+      - 09addtext.html
+      - addtext.js
+      - design9
+12. テキストボックスとプルダウンを`<li>`要素で追加する処理
+    - 2018/12/16
+      - 10addList.html
+      - addList.js
+13. サイコロを作成
+    - 2018/12/16
+      - 13dice.html
+      - design13.css
+      - dice.js
+15. npm run eslinｔ設定用にnpm install
+    - 2018/12/24
+15. 学習用にスクリプトを記載
+    - 2018/12/30
--- js/15test.js
+++ js/15test.js
@@ -15,3 +15,135 @@ console.log('==============================');
 
 const num = 1 ;
 console.log(typeof num);
+
+const str = 'こんにちは';
+console.log(typeof str);
+
+const flag = true;
+console.log(typeof flag);
+
+const ary1 = ['Javascript', 'Ajax','ASP.NET'];
+console.log(typeof ary1);
+
+const obj = {x:1 , y:2};
+console.log(typeof obj);
+
+console.log('==============================');
+
+const x = 30;
+if (x >= 10) {
+  console.log('変数xは10以上です');
+} else if (x >= 20){
+  console.log('変数xは20以上です');
+} else {
+  console.log('変数xは10未満です');
+}
+
+const x2 = 30;
+if (x2 >= 10) {
+  if (x >= 20) {
+    console.log('変数xは20以上です');
+  } else {
+    console.log('変数xは10以上20未満です');
+  }
+} else {
+  console.log('変数xは10未満です');
+}
+
+const rank = 'B';
+switch (rank) {
+case 'A' :
+  console.log('Aランクです');
+  break;
+case 'B' :
+  console.log('Bランクです');
+  break;
+case 'C' :
+  console.log('Cランクです');
+  break;
+default:
+  console.log('ランク外です');
+  break;
+}
+
+console.log('==============================');
+
+let x3 = 8;
+while (x3 < 10) {
+  console.log('x3の値は'+ x3);
+  x3++;
+}
+
+let x4 = 10;
+do {
+  console.log('x4の値は'+ x4);
+  x4++;
+} while (x4 < 10);
+
+for (let x5 = 8 ; x5 < 10; x5++){
+  console.log('x5の値は'+ x5);
+}
+
+for (let i = 1 , j = 1; i  < 5 ; i++ ,j++ ){
+  console.log('i*j:'+i*j);
+}
+
+let data3 = {apple:150 , orange:100 ,banana:120 };
+for (let key in data3) {
+  console.log(key+'='+data3[key]);
+}
+
+let data4 = ['apple' , 'orange' , 'banana'];
+Array.prototype.hoge = function(){};
+for (let key in data4) {
+  console.log(data4[key]);
+}
+
+let data5 = ['apple','orange','banana'];
+Array.prototype.hoge = function() {};
+for (let value of data5 ) {
+  console.log(value);
+}
+
+let result = 0;
+for (let i= 1 ; i <= 100 ; i++) {
+  result += i;
+  if (result > 1000){
+    console.log('合計値が1000を超えるのは'+i);
+    break;
+  }
+}
+
+kuku:
+for (let i = 1 ; i  <10 ; i++) {
+  for (let j = 1 ; j <10 ;j++) {
+    let k = i*j;
+    if (k > 30 ){
+      break kuku;
+    }
+    document.write(k+ '&nbsp');
+  }
+  document.write('<br />');
+}
+
+console.log('==============================');
+ 
+let i = 1;
+
+try {
+  i= i * j ;
+}catch (e) {
+  console.log(e.message);
+} finally {
+  console.log('処理は終了しました。');
+}
+
+let x1 = 0;
+let y = 0;
+try {
+  if (y === 0) { throw new Error('０で除算しようとした')};
+  let z = x1 /y ;
+
+} catch(e) {
+  console.log(e.message);
+}
