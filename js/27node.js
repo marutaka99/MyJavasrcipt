6b70d4e0d139158f42bb58a218548d5569bdd291
marutaka99 <maruktaka99@yahoo.co.jp>
2019-01-03 10:47:03 +0900

    27 use templete by`node.js`

--- js/27node.js
+++ js/27node.js
@@ -8,10 +8,10 @@ server.listen(1337);
 function dorequest (req, res) {
   fs.readFile('../html/27node.html','UTF-8',doRead);
   const title = 'sample page';
-  const msg = 'this is sample page prepared by program'
+  const msg = 'this is sample page prepared by program';
   function doRead(err, data) {
     const str = data.replace(/@@@title@@@/g,title)
-      .replace(/@@@message@@@/,msg)
+      .replace(/@@@message@@@/,msg);
     res.setHeader('Content-Type','text/html');
     res.write(str);
     res.end();
